08ad2ccee22a74f6cd6a2f8e2bb5fdfb57e50c7f
RAFI-290503 <rafiarnof29@gmail.com>
2026-05-20 09:47:04 +0700

    fix map

--- resources/js/Pages/Helpdesk/AdminComponents/MonitoringMap.tsx
+++ resources/js/Pages/Helpdesk/AdminComponents/MonitoringMap.tsx
@@ -62,8 +62,8 @@ const MonitoringMap: React.FC<MonitoringMapProps> = ({ dbUnits, dbCases }) => {
         return L.divIcon({
             className: 'custom-div-icon',
             html: `<div class="${hasDamage ? 'marker-pin-red' : 'marker-pin-green'}"></div>`,
-            iconSize: [14, 14],
-            iconAnchor: [7, 7]
+            iconSize: [36, 36],
+            iconAnchor: [18, 18]
         });
     };
 
@@ -194,10 +194,10 @@ const MonitoringMap: React.FC<MonitoringMapProps> = ({ dbUnits, dbCases }) => {
                         maxBoundsViscosity={1.0}
                         minZoom={5}
                     >
-                        {/* Dark Tactical Map Tiles */}
+                        {/* Bright Modern Map Tiles */}
                         <TileLayer
                             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-                            url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+                            url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
                         />
                         
                         <ZoomControl position="bottomright" />
